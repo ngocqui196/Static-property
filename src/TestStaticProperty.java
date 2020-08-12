6f94fa8dec653eb5e5b5dbb9eb6c19f6fe9ab0a3
ngocqui1996 <hivongmotloihua@yahoo.com>
2020-08-12 22:58:54 +0700

    Static-Property

---
+++ src/TestStaticProperty.java
@@ -0,0 +1,14 @@
+public class TestStaticProperty {
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+
+
+    }
+
+}
